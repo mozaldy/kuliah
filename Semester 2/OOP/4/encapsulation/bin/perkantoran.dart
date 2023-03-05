f5071b5b770167c598df1e03a3116dd2e83d9a03
potatoedz <aldy7za@gmail.com>
2023-03-05 14:48:57 +0700

    encapsulation

--- Semester 2/OOP/4/encapsulation/bin/perkantoran.dart
+++ Semester 2/OOP/4/encapsulation/bin/perkantoran.dart
@@ -1,24 +1,71 @@
 class Office {
-  String _name;
-  List<Employee> employees = [];
+  String _name; // atribut nama office
+  List<Employee> employees = []; // atribut list employee
+  static List<String> availablePositions = [
+    // posisi yang available
+    'CEO',
+    'Programmer',
+    'Akuntan',
+    'Sekretaris'
+  ];
 
-  set name(String name) => _name = name;
+  set name(String name) => _name = name; // Set nama office
 
-  String get getName => _name;
+  String get getName => _name; // Get nama office
 
-  void hire(Employee employee) {
-    employees.add(employee);
+  get getEmployees {
+    // get semua pekerja
+    print('No. \t Name \t\t\t\t Position \t Salary'); // kolom
+    int no = 1; // inisialisai nomor tabel
+    for (Employee employee in employees) {
+      // baris
+      print(
+          "$no. \t ${employee._name} \t ${employee._position} \t${employee._salary}");
+      no++; // increment no tabel untuk setiap baris
+    }
   }
 
+  void hire(Employee employee) {
+    employees.add(employee); // hire pekerja
+  }
 }
 
 class Employee {
-  String _name;
-  int _age;
-  String _gender;
-  String _position;
-  int _salary;
+  String _name; // atribut nama
+  String _position; // atribut jabatan
+  int _salary; // atribut gaji
 
-  set name(String name) => _name = name;
-  String get getName => "${this._name}";
+  bool validEmployee = true; // apabila pekerja valid
+
+  set name(String name) {
+    // set method name employee
+    if (name.length > 10) {
+      _name = name;
+    } else {
+      validEmployee = false;
+      print('Name is invalid, make sure you input the full name!');
+    }
+  }
+
+  set position(String position) {
+    // set method position employee
+    if (Office.availablePositions.contains(position)) {
+      // apabila jabatan yg di input ada di availabel positions
+      _position = position;
+    } else {
+      validEmployee = false;
+      print(
+          'Invalid position, make sure you input available positions: ${Office.availablePositions}');
+    }
+  }
+
+  set salary(int salary) {
+    // set method gaji employee
+    if (salary > 1000000) {
+      // apabila gaji diatas 1 jt
+      _salary = salary;
+    } else {
+      validEmployee = false;
+    }
+  }
 }
