30fc0f544505b96f4b3e89ad5bc2d7b05ed5ee63
Mohammad Rizaldy Ramadhan <85663081+mozaldy@users.noreply.github.com>
2023-08-25 01:00:25 +0000

    percobaan week 1

---
+++ week1/percobaan1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>  
+int main()  
+{
+    printf("Nama: Mohammad Rizaldy Ramadhan"); 
+    printf("Kelas: 1 D4 Teknik Informatika A"); 
+    printf("NRP: 3123600011"); 
+    return 0;
+}
