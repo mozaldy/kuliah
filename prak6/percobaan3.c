468646c9dfde88dc6e6076ddd0bfd503821108ad
Mohammad Rizaldy Ramadhan <85663081+mozaldy@users.noreply.github.com>
2023-09-08 02:13:15 +0000

    prak 6 & 7 done

--- prak6/percobaan3.c
+++ prak6/percobaan3.c
@@ -2,18 +2,18 @@
 
 void main()
 {
-    int angka_pertama, angka_kedua, menu, hasil;
+    float angka_pertama, angka_kedua, menu, hasil;
     printf("Masukkan bilangan pertama: ");
-    scanf("%d", &angka_pertama);
+    scanf("%f", &angka_pertama);
     printf("Masukkan bilangan kedua: ");
-    scanf("%d", &angka_kedua);
+    scanf("%f", &angka_kedua);
     printf("Menu Matematika\n");
     printf("1. Penjumlahan\n");
     printf("2. Pengurangan\n");
     printf("3. Pembagian\n");
     printf("4. Perkalian\n");
     printf("Masukkan pilihan anda: ");
-    scanf("%d", &menu);
+    scanf("%f", &menu);
 
     if (menu == 1)
     {
@@ -36,5 +36,5 @@ void main()
         printf("Menu tidak valid!\n");
         return;
     }
-    printf("Hasil dari operasi tersebut = %d\n", hasil);
+    printf("Hasil dari operasi tersebut = %.1f\n", hasil);
 }
--- prak6/percobaan4.c
+++ prak6/percobaan4.c
@@ -7,27 +7,26 @@ void main()
     scanf("%d", &angka);
     if (angka <= 40)
     {
-        printf("Nilai char huruf adalah E\n");
+        printf("Nilai huruf adalah E");
     }
     else if (angka > 40 && angka <= 55)
     {
-        printf("Nilai char huruf adalah D\n");
+        printf("Nilai huruf adalah D\n");
     }
     else if (angka > 55 && angka <= 60)
     {
-        printf("Nilai char huruf adalah C\n");
+        printf("Nilai huruf adalah C\n");
     }
     else if (angka > 60 && angka <= 80)
     {
-        printf("Nilai char huruf adalah B\n");
+        printf("Nilai huruf adalah B\n");
     }
     else if (angka > 80 && angka <= 100)
     {
-        printf("Nilai char huruf adalah A\n");
+        printf("Nilai huruf adalah A\n");
     }
     else
     {
         printf("Angka tidak valid!\n");
-        return;
     }
 }
