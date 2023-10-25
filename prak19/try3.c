a95f988f1e75df582f0daba2ac409fc7e1b1b1a2
Mohammad Rizaldy Ramadhan <85663081+mozaldy@users.noreply.github.com>
2023-10-25 07:42:38 +0000

    prak 19 done

--- prak19/try3.c
+++ prak19/try3.c
@@ -7,24 +7,22 @@ float radian(int);
 
 void main()
 {
+    printf("sin30: %f\n", sin(radian(90)));
     float data[999][999];
     int x[999] = {0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360};
     int len = sizeof(x) / sizeof(x[0]);
     for (size_t i = 0; i < 13; i++)
     {
-        int n1 = radian(x[0]);
-        int n2 = radian(x[1]);
-        int n3 = radian(x[2]);
-        data[i][0] = sin(n1);
-        data[i][1] = cos(n2);
-        data[i][2] = tan(n3);
+        data[i][0] = sin(radian(x[i]));
+        data[i][1] = cos(radian(x[i]));
+        data[i][2] = tan(radian(x[i]));
     }
     for (size_t i = 0; i < 13; i++)
     {
         printf("%d\t", x[i]);
         for (size_t j = 0; j < 3; j++)
         {
-            printf("%f\t", data[i][j]);
+            printf("%.1f\t", data[i][j]);
         }
         printf("\n");
     }
