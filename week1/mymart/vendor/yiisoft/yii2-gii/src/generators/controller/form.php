af79c8dbcc33c1918efe8f80cc76305d12e0d9ed
mozaldy <moxaldy@gmail.com>
2024-09-13 10:08:53 +0700

    Remove mymart from oop branch

--- week1/mymart/vendor/yiisoft/yii2-gii/src/generators/controller/form.php
+++
@@ -1,9 +0,0 @@
-<?php
-/** @var yii\web\View $this */
-/** @var yii\widgets\ActiveForm $form */
-/** @var yii\gii\generators\controller\Generator $generator */
-
-echo $form->field($generator, 'controllerClass');
-echo $form->field($generator, 'actions');
-echo $form->field($generator, 'viewPath');
-echo $form->field($generator, 'baseClass');
